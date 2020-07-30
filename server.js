2eda70a07557d2e7196e0dd2f8b796a66b2a0f06
KSDragon82 <www.kiarashs2929@gmail.com>
2020-07-31 01:48:19 +0430

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 if (process.env.NODE_ENV !== 'production') {
-	require('dotenv');
+	require('dotenv').config();
 }
 
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -14,6 +16,9 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/', indexRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
 mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -21,5 +26,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection;
 db.on('error', (err) => console.error(err));
 db.once('open', () => console.log('connected to mongoose'));
+
+app.use('/authors', authorsRouter);
 
 app.listen(process.env.PORT || 3000);
